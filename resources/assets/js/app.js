7ddb35e021dbdd84259c7d49b25b7e3ad096a86a
Evan Mattson <me@aaemnnost.tv>
2015-10-30 00:43:40 -0400

    big update: create/edit using modal, all updates via ajax and reactive

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,7 +17,8 @@ var vm = new Vue({
 
   data: {
     terms: [],
-    newTerm: {
+    errors: [],
+    modalTerm: {
       name: '',
       acronym: '',
       definition: ''
@@ -33,9 +34,11 @@ var vm = new Vue({
     'load-terms': function() {
       this.loadTerms();
     },
-    //'create-term': function() {
-    //  console.log(arguments);
-    //},
+    'edit-term': function(term) {
+      console.log('event::edit-term');
+      this.$data.modalTerm = _.clone(term);
+      this.termEdit();
+    },
     'destroy-term': function(term) {
       console.log('event::destroy-term');
       this.destroyTerm(term);
@@ -44,42 +47,51 @@ var vm = new Vue({
 
   methods: {
     loadTerms: function() {
-      this.$http.get('/terms', function (data, status, request) {
-        console.log('terms response', status, data);
+      this.$http.get('/term', function (data, status, request) {
+        console.log('terms', status, data);
 
         this.$set('terms', data);
-      })
-        .error(function (data, status, request) {
-        // handle error
-        console.log('Error!', status, data);
-      });
+      }).error(this.errorHandler.bind(this));
     },
 
-    createTerm: function() {
+    resetModal: function() {
+      this.$data.errors = [];
 
-      var postData = {
-        name: this.$data.newTerm.name,
-        acronym: this.$data.newTerm.acronym,
-        definition: this.$data.newTerm.definition
+      this.$data.modalTerm = {
+        name: '',
+        acronym: '',
+        definition: ''
       };
+    },
 
-      this.$http.post('/term', postData, function(data, status, request) {
-        console.log('created successfully!', status, data);
+    // show modal to create a new term
+    termCreate: function() {
+      this.$data.modalAction = 'save'; // vue-resource
+      this.$broadcast('show-modal', 'term_form');
+    },
+    // show modal to edit a term
+    termEdit: function() {
+      this.$data.modalAction = 'update'; // vue-resource
+      this.$broadcast('show-modal', 'term_form');
+    },
+    // submit modal
+    commitTerm: function() {
+      var action = this.$data.modalAction,
+        term = this.$data.modalTerm;
 
-        if (data) {
-          this.$data.terms.push(data);
-
-          this.$data.newTerm = {
-            name: '',
-            acronym: '',
-            definition: ''
-          };
+      this.$resource('term/:id')[ action ]({id: term.id}, term, function(data, status, request) {
+        console.log('commitTerm success!', action, arguments);
+        // push on the new term if it was successful
+        if (action === "save" && "object" === typeof data.term) {
+          this.$data.terms.push(data.term);
         }
-      })
-        .error(function (data, status, request) {
-          // handle error
-          console.log('Error creating term!', status, data);
-        });
+        // update the term if it was successful edit
+        if (action === "update" && "object" === typeof data.term) {
+          this.updateTerm(data.term);
+        }
+        this.resetModal();
+        this.$broadcast('hide-modal', 'term_form');
+      }).error(this.errorHandler.bind(this));
     },
 
     destroyTerm: function(term) {
@@ -87,20 +99,37 @@ var vm = new Vue({
         this.$http.delete('/term/'+ term.id, function(data, status, request) {
           console.log('deleting term', status, data);
           this.removeTerm(term);
-        })
-          .error(function (data, status, request) {
-            // handle error
-            console.log('Error destroying term!', status, data);
-          });
+        }).error(this.errorHandler.bind(this));
       }
     },
 
     removeTerm: function(term) {
-      for(var key in this.$data.terms) {
-        if (term.id == this.$data.terms[key].id) {
-          this.$data.terms.splice(key, 1);
+      this.$data.terms = _.filter(this.$data.terms, function(oldTerm) {
+        return term.id !== oldTerm.id;
+      });
+    },
+
+    updateTerm: function(term) {
+      _.forEach(this.$data.terms, function(oldTerm) {
+        if (term.id == oldTerm.id) {
+          _.merge(oldTerm, term);
         }
+      });
+    },
+
+    errorHandler: function (data, status, request) {
+      console.log('Error!', status, request, data);
+      this.$data.errors = _.values(data);
+    }
+  }, // methods
+
+  filters: {
+    toLines: function(content) {
+      console.log(content);
+      if ("object" === typeof content) {
+        return content.join('<br>');
       }
+      return content;
     }
   }
 
--- resources/views/terms/index.blade.php
+++ resources/views/terms/index.blade.php
@@ -4,34 +4,41 @@
 
     <h3>
         <span v-text="terms.length"></span> terms, and counting...
-        <button @click="$broadcast('show-modal', 'new_term')" class="btn btn-info pull-right"><i class="fa fa-plus"></i></button>
+        <button v-on:click="termCreate" class="btn btn-info pull-right"><i class="fa fa-plus"></i></button>
     </h3>
 
     <term-table :terms="terms"></term-table>
 
-    <modal id="new_term">
-        <div slot="header"><h2>Define a new term</h2></div>
-        <div slot="body">
-            <form @submit.prevent="createTerm">
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" name="name" class="form-control" v-model="newTerm.name">
-                </div>
-                <div class="form-group">
-                    <label for="acronym">Acronym</label>
-                    <input type="text" name="acronym" size="10" maxlength="10" class="form-control" v-model="newTerm.acronym">
-                </div>
-                <div class="form-group">
-                    <label for="definition">Definition</label>
-                    <textarea type="text" name="definition" class="form-control" v-model="newTerm.definition"></textarea>
-                </div>
-
-                <div class="form-group" v-show="newTerm.name && newTerm.definition">
-                    <button type="submit" class="btn btn-primary btn-large">Submit</button>
-                </div>
-            </form>
+    <modal id="term_form">
+        <div slot="header">
+            <h2>Define a new term</h2>
+            <alert v-for="error in errors" :show="!!error" state="danger" :message="error | toLines"></alert>
         </div>
-        <div slot="footer">aww yeah</slot>
+
+        <div slot="body">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name" class="form-control" v-model="modalTerm.name">
+            </div>
+            <div class="form-group">
+                <label for="acronym">Acronym</label>
+                <input type="text" name="acronym" size="10" maxlength="10" class="form-control" v-model="modalTerm.acronym">
+            </div>
+            <div class="form-group">
+                <label for="definition">Definition</label>
+                <textarea type="text" name="definition" class="form-control" v-model="modalTerm.definition"></textarea>
+            </div>
+        </div>
+
+        <div slot="footer">
+            <div class="form-group">
+                <button v-on:click="commitTerm"
+                        class="btn btn-default btn-large"
+                        :class="{ 'btn-primary': modalTerm.name && modalTerm.definition }"
+                        :disabled="modalTerm.name && modalTerm.definition ? false : 'disabled'"
+                >Submit</button>
+            </div>
+        </div>
     </modal>
 
 @stop
