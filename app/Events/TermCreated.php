62e54f31c440e9fad116576b86495a9506d1be64
Evan Mattson <me@aaemnnost.tv>
2015-10-30 23:24:31 -0400

    create Term C U D events for broadcasting

---
+++ app/Events/TermCreated.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use App\Events\Event;
+use App\Term;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class TermCreated extends Event implements ShouldBroadcast
+{
+    use SerializesModels;
+    /**
+     * @var Term
+     */
+    public $term;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param Term $term
+     */
+    public function __construct(Term $term)
+    {
+        $this->term = $term;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return ['term'];
+    }
+}
